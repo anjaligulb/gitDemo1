6abb03a2a5df3d9aa9d93081399abd352b01c278
tyss <tyss@tyss-bench-007>
2018-11-16 16:18:19 +0530

    first update

---
+++ src/sales/page1.java
@@ -0,0 +1,68 @@
+package sales;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Random;
+
+public class page1 {
+	
+		public static void main(String[] args) {
+			/*String srr[]={"abc", "xyz", "abc", "pqr","xyz","abc"};
+			LinkedHashMap<String, Integer> map=new LinkedHashMap<String, Integer>();
+			for(String s:srr)
+			{
+				if(map.containsKey(s))
+				{
+					int value=map.get(s);
+					value++;
+					map.put(s, value);
+				}
+				else
+				{
+					map.put(s,1);
+				}
+				
+			}
+			
+
+			for(String s:map.keySet())
+
+				System.out.println(s+" "+map.get(s));
+			}
+			*/
+			
+			/*Random r=new Random();
+			int rn=r.nextInt(5);
+			System.out.println(rn);*/
+			
+		//Integer[] arr={1,1,0,0,1,1,0};
+		/*Arrays.sort(arr, Collections.reverseOrder());
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			System.out.print(arr[i]+" ");
+		}*/
+	/*	
+		List<Integer> list=Arrays.asList(arr);
+		Collections.sort(list);
+		for(Integer in:list )
+		{
+			System.out.print(in+" ");
+		}*/
+	
+	
+	
+}
+}
+	
+
+
+
